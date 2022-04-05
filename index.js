5c79bac8c460448ef9f12b072243e325dab955a9
MinhajSadik <MinhajoSadik@gmail.com>
2022-04-05 12:34:53 +0600

    [ADD] learned node golabal Object & module system

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-console.log("Hello World");
-console.log("Hello SharMinaj");
-console.log("Hello Minhaj");
-console.log("Hello Sadik");
+const people = require("./people");
+
+console.log(people.people[0]);
+console.log(people.a);
+people.printName("SharMinaj");
---
+++ people.js
@@ -0,0 +1,13 @@
+const people = ["SharMinaj", "Minhaj", "Sadik"];
+
+const a = 6;
+
+function printName(name) {
+  console.log(`Hello ${name}`);
+}
+
+module.exports = {
+  people,
+  a,
+  printName,
+};
