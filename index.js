95cd2b22b2a51e9e6156eb789aae41bc99ef11b5
MinhajSadik <MinhajoSadik@gmail.com>
2022-04-05 13:21:48 +0600

    [ADD] fs module Sync & Async way implementation readFile & readFileSync

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const lodash = require("lodash");
 const path = require("path");
 const os = require("os");
+const fs = require("fs");
 
 //path module
-const myPath =
-  "/Users/minhajsadik/Documents/All-Projects/node-explore-more/index.js";
+// const myPath =
+//   "/Users/minhajsadik/Documents/All-Projects/node-explore-more/index.js";
 
-console.log(path.dirname(myPath));
-console.log(path.dirname(myPath));
-console.log(path.extname(myPath));
-console.log(path.parse(myPath));
+// console.log(path.dirname(myPath));
+// console.log(path.dirname(myPath));
+// console.log(path.extname(myPath));
+// console.log(path.parse(myPath));
 
 //os module
-console.log(os.platform());
-console.log(os.platform());
-console.log(os.freemem());
-console.log(os.cpus());
+// console.log(os.platform());
+// console.log(os.homedir());
+// console.log(os.freemem());
+// console.log(os.cpus());
 
+//fs module
+// fs.writeFile("notes.txt", "This is my first note, ");
+// // fs.writeFileSync("notes.txt", "This is my note 2"); //this will overwrite the previous note
+
+// fs.appendFile("notes.txt", "This is my note 2 appended");
+
+fs.readFile("notes.txt", "utf8", (err, data) => {
+  if (err) {
+    console.log("Error: ", err);
+  } else {
+    console.log("Data: ", data);
+  }
+});
+
+console.log("After reading the file");
