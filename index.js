992c5aa5958e88f35f4aff33f38f525ea416f438
MinhajSadik <MinhajoSadik@gmail.com>
2022-04-07 01:34:24 +0600

    [ADD] working for file system

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const path = require("path");
 const os = require("os");
 const fs = require("fs");
+const { chunk } = require("lodash");
 // const EventEmitter = require("events");
-const Periods = require("./external");
+// const Periods = require("./external");
 //path module
 // const myPath =
 //   "/Users/minhajsadik/Documents/All-Projects/node-explore-more/index.js";
@@ -20,27 +21,27 @@ const Periods = require("./external");
 
 //fs module
 // fs.writeFile("notes.txt", "This is my first note, ");
-fs.writeFile("notes.txt", "This is my first note, ", (err) => {
-  if (err) {
-    console.log("Unable to write file");
-  } else {
-    console.log("File written successfully");
-  }
-});
+// fs.writeFile("notes.txt", "This is my first note, ", (err) => {
+//   if (err) {
+//     console.log("Unable to write file");
+//   } else {
+//     console.log("File written successfully");
+//   }
+// });
 
 // // fs.writeFileSync("notes.txt", "This is my note 2"); //this will overwrite the previous note
 
 // fs.appendFile("notes.txt", "This is my note 2 appended");
 
-fs.readFile("notes.txt", "utf8", (err, data) => {
-  if (err) {
-    console.log("Error: ", err);
-  } else {
-    console.log("Data: ", data);
-  }
-});
+// fs.readFile("notes.txt", "utf8", (err, data) => {
+//   if (err) {
+//     console.log("Error: ", err);
+//   } else {
+//     console.log("Data: ", data);
+//   }
+// });
 
-console.log("After reading the file");
+// console.log("After reading the file");
 
 //EventEmitter module
 // const emitter = new EventEmitter();
@@ -51,11 +52,17 @@ console.log("After reading the file");
 // });
 
 // which Object created for emiting same object need for listening this event otherwise it won't works recognized
-const period = new Periods();
-period.on("periodEvent", ({ period, text }) => {
-  console.log(`Event Fired for Period Start ${period} ${text}`);
-});
-period.startPeriod();
+// const period = new Periods();
+// period.on("periodEvent", ({ period, text }) => {
+//   console.log(`Event Fired for Period Start ${period} ${text}`);
+// });
+// period.startPeriod();
 
 //emit the event "message" it means raise the event
 // emitter.emit("event");
+
+const ourReadStreem = fs.createReadStream(`${__dirname}/notes.txt`, "utf-8");
+
+ourReadStreem.on("data", (data) => {
+  console.log(data);
+});
