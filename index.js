8f5b0e6f6344abf6a11ff44b142f173d0c7c40e2
MinhajSadik <MinhajoSadik@gmail.com>
2022-04-12 05:27:14 +0600

    [ADD] writeStream learned deeper

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("<html><head><title>Form</title></head>");
-    res.write(
-      `<body><form method='post' action="/process"><input name='message' /></form></body></html>`
-    );
-    res.end();
-  } else if (req.url === "/process" && req.method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      console.log("stream finished");
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody);
-      res.write("Thanks For Submitting");
-      res.end();
-    });
-  } else {
-    res.write("Not Found");
-    res.end();
-  }
+const ourReadStreem = fs.createReadStream(
+  `${__dirname}/Core/output.txt`,
+  "utf8"
+);
+const ourWriteStreem = fs.createWriteStream(`${__dirname}/output.txt`, "utf8");
+
+ourReadStreem.on("data", (chunk) => {
+  ourWriteStreem.write(chunk);
 });
 
-// server.on("connection", (socket) => {
-//   console.log("New connection");
-// });
-
-server.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+console.log("hello");
