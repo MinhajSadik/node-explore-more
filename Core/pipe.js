9307fbfdcc5164da7eae5008f545b8863d6d758d
MinhajSadik <MinhajoSadik@gmail.com>
2022-04-12 05:45:39 +0600

    [ADD/DONE] pipe code move on pipe.js file

--- Core/pipe.js
+++ Core/pipe.js
@@ -13,3 +13,25 @@ const ourWriteStreem = fs.createWriteStream(`${__dirname}/output.txt`, "utf8");
 ourReadStreem.pipe(ourWriteStreem); // same things using pipe method
 
 console.log("hello");
+
+// 2nd easy way
+
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  //req readable stream, res writable stream
+  const myReadStream = fs.createReadStream(
+    __dirname + "/Core/output.txt",
+    "utf8"
+  );
+  myReadStream.pipe(res);
+});
+
+// server.on("connection", (socket) => {
+//   console.log("New connection");
+// });
+
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
