a14cdd765d769112704e6ab26d04cf3b2e6ad56b
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-01 00:36:14 +0600

    [ADD] working and learning node express functions which is core in api reference

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+//raw: we'll get buffer binary data, we've to convert it to string and then to json
+// app.use(express.raw());
+// app.use(express.text());
+// app.use(express.urlencoded());
+app.use(
+  express.static(__dirname + "/public", {
+    extensions: ["html"],
+    index: "home.html",
+  })
+);
+const router = app.use(express.Router());
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
   res.send("This is home pages!");
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
+router.post("/", (req, res) => {
+  console.log(req.body.name);
   res.send("This Is Home Page with Post Request");
 });
 
