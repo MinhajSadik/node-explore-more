e1f27eee8e04f9d77a9d5514d0d61c3c82446226
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-03 21:48:49 +0600

    [ADD] file uploading everything learning with multer

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require("express");
+const multer = require("multer");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  console.log("Hello");
+// File Upload Folder
+const UPLOADS_FOLDER = "./uploads";
+
+// Prepare the final multer upload object
+let upload = multer({
+  dest: UPLOADS_FOLDER,
 });
+
+//single file upload
+// app.post("/", upload.single("avater"), (req, res) => {
+//   res.send("Hello");
+// });
+
+//multiple file upload
+// app.post("/", upload.array("avater", 3), (req, res) => {
+//   res.send("Hello");
+// });
+
+//
+app.post(
+  "/",
+  upload.fields([
+    { name: "avater", maxCount: 1 },
+    { name: "gallery", maxCount: 3 },
+  ]),
+  (req, res) => {
+    res.send("Hello");
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
