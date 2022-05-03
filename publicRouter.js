c1e643227b9c889ebcb703da1da265395e4cb859
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-03 11:38:02 +0600

    [ADD] routing system

--- publicRouter.js
+++ publicRouter.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const publicRouter = express.Router();
 
+publicRouter.param("user", (req, res, next, user) => {
+  req.user = user === "1" ? "Admin" : "User";
+  next();
+});
+
+publicRouter.get("/:user", (req, res, next) => {
+  console.log(`Public page for ${req.user}`);
+  next();
+});
+
 publicRouter.get("/", (req, res) => {
   res.send("Home page");
 });
