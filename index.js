880f6a80b6f7257fcce4c6aa6a0ea323a428931c
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-04 01:37:20 +0600

    [ADD] mongodb learning for re-practice

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Learning MongoDB Commands");
 });
 
 app.listen(PORT, () => {
