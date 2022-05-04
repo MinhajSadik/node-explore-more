ff7571d75ea14d5ca419054775603d4b2cedf885
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-04 12:41:34 +0600

    [ADD] learning with mongoose CRUD commands

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const mongooose = require("mongoose");
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
+//database connetion with mongoose
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongooose
+  .connect("mongodb://localhost:27017/todos", options)
+  .then(() => console.log("Connected to Database..."))
+  .catch((err) => console.error("Could not connect to Ddatabase...", err));
+
+//application routes
 app.get("/", (req, res) => {
   res.send("Learning MongoDB Commands");
 });
+
+//default error handler
+const errorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500).json({ error: err.message });
+};
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
