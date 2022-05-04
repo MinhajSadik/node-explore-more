584e2d0a3be87abc5153fccb579cad6efde06caa
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-05 02:09:19 +0600

    [ADD] CRUD working finished

--- routes/todoHandler.js
+++ routes/todoHandler.js
@@ -5,19 +5,72 @@ const Todo = require("../schemas/todoSchema");
 
 //get all todos
 router.get("/", async (req, res) => {
-  Todo.find({}, (err, todos) => {
-    if (err) {
-      res.status(500).json({
-        error: err,
-      });
-    } else {
-      res.status(200).send(todos);
-    }
-  });
+  // Todo.find({}, (err, todos) => {
+  //   if (err) {
+  //     res.status(500).json({
+  //       error: err,
+  //     });
+  //   } else {
+  //     res.status(200).json({
+  //       todos,
+  //       message: "Successfully fetched all todos",
+  //     });
+  //   }
+  // });
+  await Todo.find({})
+    .select({
+      _id: 0,
+      __v: 0,
+      date: 0,
+    })
+    .limit(5)
+    .exec((err, todos) => {
+      if (err) {
+        res.status(500).json({
+          error: err.message,
+        });
+      } else {
+        res.status(200).json({
+          todos,
+          message: "Successfully fetched all todos",
+        });
+      }
+    });
 });
 
 //get a todo by ID
-router.get("/:id", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+  // Todo.find({ _id: req.params.id }, (err, todo) => {
+  //   if (err) {
+  //     res.status(500).json({
+  //       error: err,
+  //     });
+  //   } else {
+  //     res.status(200).json({
+  //       todo,
+  //       message: "Successfully fetched todo",
+  //     });
+  //   }
+  // });
+  Todo.find({ _id: req.params.id })
+    .select({
+      _id: 0,
+      __v: 0,
+      date: 0,
+    })
+    .exec((err, todo) => {
+      if (err) {
+        res.status(500).json({
+          error: err.message,
+        });
+      } else {
+        res.status(200).json({
+          todo,
+          message: "Successfully found todo by ID",
+        });
+      }
+    });
+});
 
 //post a todos
 router.post("/", async (req, res) => {
@@ -98,6 +151,18 @@ router.put("/:id", (req, res) => {
 });
 
 //get all todos
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  Todo.deleteOne({ _id: req.params.id }, (err) => {
+    if (err) {
+      res.status(500).json({
+        error: err,
+      });
+    } else {
+      res.status(200).json({
+        message: "todo was deleted successfully",
+      });
+    }
+  });
+});
 
 module.exports = router;
