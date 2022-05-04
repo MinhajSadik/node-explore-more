cced051b8524dcae1c082aa54d1d82c8dbe35f59
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-05 01:17:11 +0600

    [ADD] put method working done

--- routes/todoHandler.js
+++ routes/todoHandler.js
@@ -5,7 +5,15 @@ const Todo = require("../schemas/todoSchema");
 
 //get all todos
 router.get("/", async (req, res) => {
-  res.send("Get all todos");
+  Todo.find({}, (err, todos) => {
+    if (err) {
+      res.status(500).json({
+        error: err,
+      });
+    } else {
+      res.status(200).send(todos);
+    }
+  });
 });
 
 //get a todo by ID
@@ -43,7 +51,28 @@ router.post("/all", async (req, res) => {
 });
 
 //put todos
-router.put("/:id", async (req, res) => {});
+router.put("/:id", (req, res) => {
+  Todo.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        title: req.body.title,
+        status: req.body.status,
+      },
+    },
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          error: err.message,
+        });
+      } else {
+        res.status(200).json({
+          message: "Todo was updated successfully",
+        });
+      }
+    }
+  );
+});
 
 //get all todos
 router.delete("/:id", async (req, res) => {});
